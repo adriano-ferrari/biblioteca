3b2d036ce9ea78a7e072ee471046026735541e15
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2023-03-03 18:20:18 -0300

    Criado categoria, linkado usuário e listando livros

--- livro/views.py
+++ livro/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 from usuarios.models import Usuario
+from .models import Livros
 
 # Create your views here.
 
 def home(request):
     if request.session.get('usuario'):
-        usuario = Usuario.objects.get(id=request.session['usuario']).nome
-        return HttpResponse(f'Olá, {usuario}!')
+        usuario = Usuario.objects.get(id=request.session['usuario'])
+        livros = Livros.objects.filter(usuario=usuario)
+        return render(request, 'home.html', {'livros':livros})
     else:
         return redirect('/auth/login/?status=2')
     
+
+def visualiza(request, id):
+    livro = Livros.objects.get(id=id)
+    return render(request, 'visualiza.html', {'livro':livro})
